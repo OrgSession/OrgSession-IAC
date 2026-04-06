cc3aeffeba1a5c1ed748bb9c6aa5dbc5fccefeca
Dhanunjaya235 <120103263+Dhanunjaya235@users.noreply.github.com>
2026-04-06 12:21:16 +0530

    Add S3 module with bucket resource and outputs

---
+++ modules/s3/main.tf
@@ -0,0 +1,8 @@
+resource "aws_s3_bucket" "demo_bucket" {
+  bucket = "orgsession-demo-bucket-${var.env}"
+
+  tags = {
+    Name        = "orgsession-demo-bucket-${var.env}"
+    Environment = var.env
+  }  
+}
