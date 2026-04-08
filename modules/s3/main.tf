cd2ecabcce6489a50d3b95ad95c83feb1326994a
Dhanunjaya235 <120103263+Dhanunjaya235@users.noreply.github.com>
2026-04-08 13:33:01 +0530

    removed unnecessary s3 buckets

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -6,20 +6,3 @@ resource "aws_s3_bucket" "demo_bucket" {
     Environment = var.env
   }
 }
-
-resource "aws_s3_bucket" "demo_bucket_v2" {
-  bucket = "orgsession-demo-bucket-${var.env}-v2"
-
-  tags = {
-    Name        = "orgsession-demo-bucket-${var.env}-v2"
-    Environment = var.env
-  }
-}
-resource "aws_s3_bucket" "demo_bucket_v3" {
-  bucket = "orgsession-demo-bucket-${var.env}-v3"
-
-  tags = {
-    Name        = "orgsession-demo-bucket-${var.env}-v3"
-    Environment = var.env
-  }
-}
